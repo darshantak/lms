0813007a88c7e6b88219bddc483a7ba266853f16
Darshan Mohan Tak <darshan.mohan@juspay.in>
2024-07-23 18:02:13 +0530

    testing video player

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import ReactPlayer from 'react-player';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      
+      <ReactPlayer 
+        url = "https://www.youtube.com/watch?v=Mx92lTYxrJQ"
+        volume = "0.4"
+        
+      />
     </div>
   );
 }
